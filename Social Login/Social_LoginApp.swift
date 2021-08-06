bb7013e26788426beea493ebcc703177af55595b
Martin <martinclaesson@me.com>
2021-08-06 13:33:34 +0200

    Initial Commit

---
+++ Social Login/Social_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_LoginApp.swift
+//  Social Login
+//
+//  Created by Martin Claesson on 2021-08-06.
+//
+
+import SwiftUI
+
+@main
+struct Social_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
